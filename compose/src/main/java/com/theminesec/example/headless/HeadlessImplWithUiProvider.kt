04c71b6bd49d9bbfc738d5e25b817a278b696655
mario-minesec <mario.gamal@minesecsoftpos.com>
2026-06-18 12:16:13 +0300

    Add PinPadConfig sample

--- compose/src/main/java/com/theminesec/example/headless/HeadlessImplWithUiProvider.kt
+++ compose/src/main/java/com/theminesec/example/headless/HeadlessImplWithUiProvider.kt
@@ -1,6 +1,6 @@
 package com.theminesec.example.headless
 
-import android.util.Log
+import android.content.Context
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,18 +12,30 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import com.theminesec.lib.dto.common.Amount
 import com.theminesec.lib.dto.transaction.PaymentMethod
-
 import com.theminesec.sdk.headless.HeadlessActivity
+import com.theminesec.sdk.headless.ui.ButtonsColor
+import com.theminesec.sdk.headless.ui.PinEntryDetails
+import com.theminesec.sdk.headless.ui.PinEntryMode
+import com.theminesec.sdk.headless.ui.PinPadConfig
+import com.theminesec.sdk.headless.ui.PinPadStyle
+import com.theminesec.sdk.headless.ui.TextStyleConfig
 import com.theminesec.sdk.headless.ui.ThemeProvider
 import com.theminesec.sdk.headless.ui.UiProvider
 import com.theminesec.sdk.headless.ui.UiState
-import kotlinx.coroutines.delay
-import kotlin.time.Duration.Companion.seconds
+import com.theminesec.sdk.headless.util.PinPadStylesUtil
+
 
 class HeadlessImplWithUiProvider : HeadlessActivity() {
     override fun provideTheme(): ThemeProvider = ClientHeadlessThemeProvider()
     override fun provideUi(): UiProvider = ClientUiProvider()
 
+    // Option #1 change pinEntryMode
+    override val pinPadConfig: PinPadConfig = PinPadConfig(pinEntryMode = PinEntryMode.MOVING.name)
+
+    // Option #2 change pinEntryMode, pinEntryDetails and pinPadStyle
+    /*override val pinPadConfig: PinPadConfig by lazy {
+        getPinPadConfig(this, PinEntryMode.FIXED)
+    }*/
 }
 
 class ClientHeadlessThemeProvider : ThemeProvider() {
@@ -92,3 +104,43 @@ class ClientUiProvider : UiProvider() {
         }
     }
 }
+
+fun getPinPadConfig(context: Context, mode: PinEntryMode) =
+    PinPadConfig(
+        pinEntryMode = mode.name,
+        pinEntryDetails = PinEntryDetails(
+            pinDescription = "Your PIN Please!",
+            amount = "100.75 USD",
+            description = "Please pay MineSec monthly invoice",
+            supportPINBypass = false
+        ),
+        pinPadStyle = PinPadStyle(
+            backgroundColor = Color.LightGray.toArgb(),
+            amountStyle = getOswaldBold30(context),
+            digitStyle = getOswaldBold30(context),
+            descriptionStyle = getJbmMedium25(context),
+            buttonsColor = ButtonsColor(
+                abortBtn = Color.DarkGray.toArgb(),
+                confirmBtn = Color.Blue.toArgb(),
+                clearBtn = Color.Red.toArgb(),
+            )
+        )
+    )
+
+fun getJbmMedium25(context: Context) = TextStyleConfig(
+    fontSize = 25f,
+    fontFileUri = PinPadStylesUtil.getFontUri(
+        context = context,
+        fontResId = R.font.jbm_medium,
+        fileName = "jbm_medium.ttf"
+    )?.toString()
+)
+
+fun getOswaldBold30(context: Context) = TextStyleConfig(
+    fontSize = 30f,
+    fontFileUri = PinPadStylesUtil.getFontUri(
+        context = context,
+        fontResId = R.font.oswald_bold,
+        fileName = "oswald_bold.ttf"
+    )?.toString()
+)
