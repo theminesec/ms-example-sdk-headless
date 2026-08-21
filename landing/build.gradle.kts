7dddbe91acf22dd068666edbae153e8e402a5b98
eric.song <eric.song@theminesec.com>
2026-08-21 16:57:07 +0800

    updated

--- landing/build.gradle.kts
+++ landing/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
