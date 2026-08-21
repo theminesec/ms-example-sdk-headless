7bb405b8ce3f7bb9eb155a4ebc1637525b82299b
eric.song <eric.song@theminesec.com>
2026-08-21 18:00:12 +0800

    Upgraded

--- msa-ui/build.gradle.kts
+++ msa-ui/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.theminesec.example.headless"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.theminesec.example.headless.compose.msaui"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
--- xml-java/build.gradle.kts
+++ xml-java/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.theminesec.example.headless"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.theminesec.example.headless.xml.java"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
